c4f6a6d1269b71dfc6cae39b969a6d592fff5337
Furkan <furkancosar2005@gmail.com>
2024-10-27 12:32:43 +0300

    hotfix

--- components/header/add-feedback-button/index.jsx
+++ components/header/add-feedback-button/index.jsx
@@ -1,15 +1,11 @@
-"use client"
-
 export default function AddFeedbackButton() {
-    const [open, setOpen] = useState(false);
-
     return (
         <>
             <form action="" className="add-feedback-button">
-                <button onClick={() => setOpen(true)}>+ Add Feedback</button>
+                <button>+ Add Feedback</button>
             </form>
             
-            <div className={open ? "dialog active" : "dialog"} style={{ width: open ? "100%" : "0%" }}>
+            <div>
                 <div className="dialog-content">
                     <p>deneme modal</p>
                 </div>
