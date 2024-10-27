0d4d0ef021fb30f4e3d86036c120b1968bde8255
kemalozlp <ozlpkemal@gmail.com>
2024-10-28 02:22:35 +0300

    beğeni eklendi

--- components/suggestions-page/feedback-like/index.jsx
+++ components/suggestions-page/feedback-like/index.jsx
@@ -1,29 +1,26 @@
 "use client"
+import { UpVote } from "@/helpers/icons";
 import "./feedback-like.css";
 import React, { useState } from 'react';
 
-export default  function LikeButton() {
-   
-    const [likes, setLikes] = useState(112);  
-    const [liked, setLiked] = useState(false); 
+export default function LikeButton() {
+
+    const [likes, setLikes] = useState(112);
+    const [liked, setLiked] = useState(false);
 
     const handleLike = () => {
         if (liked) {
-            setLikes(likes - 1); 
+            setLikes(likes - 1);
         } else {
-            setLikes(likes + 1); 
+            setLikes(likes + 1);
         }
-        setLiked(!liked); 
+        setLiked(!liked);
     };
 
     return (
-        <div className="like-and-comments">
-            <div className="likes">
-                <button onClick={handleLike} style={{ color: liked ? 'red' : 'gray' }}>
-                    {liked ? 'Beğenildi' : 'Beğen'}
-                </button>
-                <p>{likes}</p>
-            </div>
-        </div>
+        <button className="likes" onClick={handleLike}>
+            <UpVote />
+            {likes}
+        </button>
     );
 }
