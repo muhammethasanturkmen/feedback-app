d8c5ee9823fc55f3a1fb1243a550b8f1852f1fbf
kemalozlp <ozlpkemal@gmail.com>
2024-10-28 02:22:03 +0300

    card kısmında css hatası düzeltildi

--- components/suggestions-page/feedback-card/index.jsx
+++ components/suggestions-page/feedback-card/index.jsx
@@ -6,23 +6,21 @@ import LikeButton from "../feedback-like";
 export default function Card() {
     return (
         <div className="card">
-            <div className="card-texts">
-                <h4>Add tags for solutions</h4>
-                <p>Easier to search for solutions based on a specific stack.</p>
-            </div>
-
-            <p className="card-category">Enhancement</p>
-
-            <div className="like-and-comments">
-                <div className="likes">
-                    <UpVote />
+            <div className="cardDetail">
+                <div className="like-and-comments">
                     <LikeButton />
                 </div>
 
-                <div className="comments">
-                    <Comments />
-                    <p>2</p>
+                <div className="card-texts">
+                    <h4>Add tags for solutions</h4>
+                    <p>Easier to search for solutions based on a specific stack.</p>
+                    <p className="card-category">Enhancement</p>
                 </div>
+            </div>
+
+            <div className="comments">
+                <Comments />
+                <p>2</p>
             </div>
         </div>
     )
